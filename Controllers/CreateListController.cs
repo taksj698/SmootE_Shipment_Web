32c46c6fb1ce72e3a8d031293c9d34570394c340
taksil <taksiljo@tlt.co.th>
2024-07-02 00:54:10 +0700

    update

---
+++ Controllers/CreateListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpeedTime.Controllers
+{
+	public class CreateListController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Controllers/ShipmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpeedTime.Controllers
+{
+	public class ShipmentController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
