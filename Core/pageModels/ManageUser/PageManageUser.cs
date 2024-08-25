260b33675a2be703573a36c7313ec387dfd2db62
Taksil Jongtap <Taksil_Jongtap@hotmail.com>
2024-08-26 01:14:34 +0700

    update

---
+++ Core/pageModels/ManageUser/PageManageUser.cs
@@ -0,0 +1,43 @@
+﻿using Document_Control.Core.dbModels;
+using Document_Control.Core.pageModels.ManagePosition;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace Document_Control.Core.pageModels.ManageUser
+{
+    public class PageManageUser
+    {
+        public List<DataUser>? tbUser { get; set; }
+        public PageManageUserSave? pageManageUserSave { get; set; }
+    }
+
+    public class DataUser : TbUser 
+    {
+        public string? PositionName { get; set; }
+        public string? RoleName { get; set; }
+        public bool IsCanDelete { get; set; }
+    }
+    public class PageManageUserSave
+    {
+        public int? Id { get; set; }
+        [Required(ErrorMessage = "ระบุชื่อผู้ใช้งาน")]
+        public string? UserName { get; set; }
+        //[Required(ErrorMessage = "ระบุชื่อรหัสผ่าน")]
+        //public string? Password { get; set; }
+        [Required(ErrorMessage = "ระบุชื่อ")]
+        public string? Name { get; set; }
+        public string? TelNo { get; set; }
+        [Required(ErrorMessage = "ระบุตำแหน่ง")]
+        public int? PositionId { get; set; }
+        public bool? IsApprove { get; set; } = false;
+        public bool? IsManager { get; set; } = false;
+        [Required(ErrorMessage = "ระบุสิทธิ์การใช้งาน")]
+        public int? RoleId { get; set; }
+
+
+
+        public List<SelectListItem>? lPosition { get; set; }
+        public List<SelectListItem>? lRole { get; set; }
+    }
+}
